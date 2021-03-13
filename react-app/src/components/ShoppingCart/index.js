fe642bdb1384863b9c68b42aa85f17ff278a8d21
River <cosmos.cha1997@gmail.com>
2021-03-13 06:43:41 -0500

    more styling on checkout page and address form

--- react-app/src/components/ShoppingCart/index.js
+++ react-app/src/components/ShoppingCart/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, Redirect } from "react-router-dom";
+import { NavLink, Redirect, useHistory } from "react-router-dom";
 import { buyProducts } from "../../store/shoppingCart";
 import "./cart.css";
 const ShoppingCart = () => {
@@ -8,28 +8,16 @@ const ShoppingCart = () => {
   const user = useSelector((state) => state.session.user);
   const loaded = useSelector((state) => state.session.loaded);
   const checkout = useSelector((state) => state.checkout.products);
+  const history = useHistory();
   let getItems = Object.values(sessionStorage);
   console.log("getItem", getItems);
   useEffect(() => {
     dispatch(buyProducts(getItems));
   }, [dispatch]);
 
-  let newArr;
-  let evenNewerArr;
-  if (checkout) {
-    if (checkout.length) {
-      newArr = checkout.map((product, i) => {
-        return [product, getItems[i]];
-      });
-
-      evenNewerArr = newArr.map((i) => {
-        console.log("information on the product", i[0]);
-        console.log("information on the quanitity", JSON.parse(i[1]).quantity);
-      });
-    }
-  }
-
-  console.log("1111111111111111111111", newArr);
+  const goToCheckout = (e) => {
+    history.push("/checkout");
+  };
 
   const checkoutMapper = (arr1, arr2) => {
     let newArr;
@@ -46,9 +34,8 @@ const ShoppingCart = () => {
             <>
               <div key={i.id}>
                 <div>{i[0].name}</div>
-                {/* <div>{photo[0]}</div> */}
-                <img src={photo[0].photoKey} />
-                <div>{i[0].price}</div>
+                <img src={photo[0].photoKey} className="productImages2" />
+                <div>{i[0].price * quant}</div>
                 <div>{quant}</div>
               </div>
             </>
@@ -58,7 +45,7 @@ const ShoppingCart = () => {
     }
     return newArr;
   };
-  console.log("qqqqqqqqqqqqqqqqqq", checkoutMapper(checkout, getItems));
+
   return loaded && user ? (
     <>
       <div className="pages-container">
@@ -68,7 +55,7 @@ const ShoppingCart = () => {
         </div>
         <div> hullo world</div>
         <div>
-          <button>checkout</button>
+          <button onclick={goToCheckout}>checkout</button>
         </div>
       </div>
     </>
