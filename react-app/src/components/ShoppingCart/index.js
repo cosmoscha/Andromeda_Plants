55496532b5c189fdebfca2fd47451b2a681255d7
River <cosmos.cha1997@gmail.com>
2021-03-13 06:15:52 -0500

    some debugging and refactoring of shopping cart feature

--- react-app/src/components/ShoppingCart/index.js
+++ react-app/src/components/ShoppingCart/index.js
@@ -15,28 +15,55 @@ const ShoppingCart = () => {
   }, [dispatch]);
 
   let newArr;
+  let evenNewerArr;
   if (checkout) {
     if (checkout.length) {
       newArr = checkout.map((product, i) => {
         return [product, getItems[i]];
+      });
+
+      evenNewerArr = newArr.map((i) => {
+        console.log("information on the product", i[0]);
+        console.log("information on the quanitity", JSON.parse(i[1]).quantity);
       });
     }
   }
 
   console.log("1111111111111111111111", newArr);
 
-  // const checkoutMapper = (arr) => {
-  //   arr = arr.map((product) => {
-  //     console.log("product", product);
-  //   });
-  //   return arr;
-  // };
-  // console.log("qqqqqqqqqqqqqqqqqq", checkoutMapper(newArr));
+  const checkoutMapper = (arr1, arr2) => {
+    let newArr;
+    if (checkout) {
+      if (checkout.length) {
+        newArr = arr1.map((product, i) => {
+          return [product, arr2[i]];
+        });
+        newArr = newArr.map((i) => {
+          const quant = JSON.parse(i[1]).quantity;
+          const photo = i[0].photos;
+          console.log("photo", photo);
+          return (
+            <>
+              <div key={i.id}>
+                <div>{i[0].name}</div>
+                {/* <div>{photo[0]}</div> */}
+                <img src={photo[0].photoKey} />
+                <div>{i[0].price}</div>
+                <div>{quant}</div>
+              </div>
+            </>
+          );
+        });
+      }
+    }
+    return newArr;
+  };
+  console.log("qqqqqqqqqqqqqqqqqq", checkoutMapper(checkout, getItems));
   return loaded && user ? (
     <>
       <div className="pages-container">
         <div>
-          {/* <div>{checkoutMapper(checkout)}</div> */}
+          <div>{checkoutMapper(checkout, getItems)}</div>
           <div></div>
         </div>
         <div> hullo world</div>
