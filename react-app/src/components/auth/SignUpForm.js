c9c841e233fcdd8be56c84cf6480a791d3d7a9fc
River <cosmos.cha1997@gmail.com>
2021-03-18 02:16:52 -0400

    added profile photo

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Redirect } from "react-router-dom";
 import { signUp } from "../../services/auth";
 import "./auth.css";
+import { CloudUploadOutlined } from "@material-ui/icons";
 const SignUpForm = ({ authenticated, setAuthenticated }) => {
+  const fileInput = useRef(null);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [profilePhotoFile, setProfilePhotoFile] = useState("");
+  const [selectedFile, setSelectedFile] = useState("Profile Image");
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const user = await signUp(username, email, password);
+      const user = await signUp(username, email, password, profilePhotoFile);
       if (!user.errors) {
         setAuthenticated(true);
       }
@@ -32,6 +36,15 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
 
   const updateRepeatPassword = (e) => {
     setRepeatPassword(e.target.value);
+  };
+
+  const updateProfilePhotoFile = (e) => {
+    setProfilePhotoFile(e.target.files[0]);
+    if (!e.target.files.length) {
+      setSelectedFile("Profile Image");
+    } else {
+      setSelectedFile(`${e.target.value.split("\\").pop()}`);
+    }
   };
 
   if (authenticated) {
@@ -79,6 +92,40 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
               value={repeatPassword}
               required={true}
             ></input>
+          </div>
+          <div>
+            <div className="normalize-text file-input ">
+              <label
+                className="upload-button"
+                onClick={() => fileInput.current.click()}
+              >
+                <div
+                  className="flex-container"
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "0",
+                    padding: "0 8px",
+                  }}
+                >
+                  <CloudUploadOutlined style={{ marginRight: "12px" }} />
+                  <h5
+                    className="normalize-text"
+                    style={{ margin: "0", overflow: "hidden" }}
+                  >
+                    {selectedFile}
+                  </h5>
+                </div>
+              </label>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                name="user_file"
+                onChange={updateProfilePhotoFile}
+                ref={fileInput}
+                //  value={profilePhotoUrl}
+              />
+            </div>
           </div>
           <button type="submit">Sign Up</button>
         </form>
