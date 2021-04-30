2eacb231aec7542f25653be0b9f49a901a594dc5
river <cosmos.cha1997@gmail.com>
2021-04-30 02:33:48 -0400

    had to do fresh install

--- react-app/src/components/IndividualProduct/index.js
+++ react-app/src/components/IndividualProduct/index.js
@@ -193,7 +193,7 @@ sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
 recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
 minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
 quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+fugiat, temporibus enim commodsi iusto libero magni deleniti quod quam 
 consequuntur! Commodi minima excepturi repudiandae velit hic maxime
 doloremque. Quaerat provident commodi consectetur veniam similique ad 
 earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
